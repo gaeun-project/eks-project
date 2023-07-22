179a4b813899ba64cd35e7fd7f205e5c2255789c
rlarkdms <rkdms7220@naver.com>
2023-07-23 02:31:49 +0900

    feat : add argoworkflows parallel processing

--- features/resources/Describe_Resource/Describe_Resource.sh
+++ features/resources/Describe_Resource/Describe_Resource.sh
@@ -4,16 +4,18 @@
 # ${parameter-role} is Role Name
 # ${parameter-region} is Region
 # ${parameter-service} i Service
-parameter-role
-parameter-region
-parameter-service
-parameter-account
-ROLE_ARN="arn:aws:iam::${parameter-account}:role/${parameter-role}"
-SESSION_NAME=${parameter-role}
-REGION=${parameter-region}
+
+echo "parameter-account: $account"
+echo "parameter-role: $role"
+echo "parameter-region: $region"
+echo "parameter-service: $service"
+
+ROLE_ARN="arn:aws:iam::${account}:role/${role}"
+SESSION_NAME=${role}
+AWS_REGION=${region}
 
 # Role을 가정하고 임시 보안 자격 증명 얻기
-CREDENTIALS=$(aws sts assume-role --role-arn $ROLE_ARN --role-session-name $SESSION_NAME --region $REGION --output json)
+CREDENTIALS=$(aws sts assume-role --role-arn $ROLE_ARN --role-session-name $SESSION_NAME --region $AWS_REGION --output json)
 
 # 임시 보안 자격 증명을 환경 변수로 설정
 export AWS_ACCESS_KEY_ID=$(echo $CREDENTIALS | jq -r .Credentials.AccessKeyId)
@@ -41,5 +43,5 @@ hours=$(date +%H)
 minutes=$(date +%M)
 seconds=$(date +%S)
 
-aws s3 cp instances.json s3://mad-master-bucket/${parameter-service}/account=${parameter-account}/service=ec2/year=${year}/month=${month}/day=${day}/hours=${hours}/AWS_Info.json
+aws s3 cp instances.json s3://mad-master-bucket/${service}/account=${account}/service=ec2/year=${year}/month=${month}/day=${day}/hours=${hours}/AWS_Info.json
 rm output.json instances.json
